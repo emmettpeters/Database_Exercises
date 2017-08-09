4f925064b3a9e380aea69b47d6b56f28782d71bf
Emmett Peters <ejp8611@gmail.com>
2017-08-09 11:45:18 -0500

    finished the third part of functions exercise

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -30,11 +30,11 @@ where last_name like "e%"
 
 /* select * from employees where birth_date like "%-12-25"; */
 
-/* select *
+select datediff(hire_date,now())
 from employees
 where (hire_date like "199%"
 	and birth_date like "%-12-25")
-	order by birth_date, hire_date desc; */
+	order by birth_date, hire_date desc;
 
 /* select * from employees where last_name like "%q%"; */
 
