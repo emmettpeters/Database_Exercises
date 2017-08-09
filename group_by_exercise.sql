7748c9bbc23ffac542475a3367ff22fd0dcfaa74
Emmett Peters <ejp8611@gmail.com>
2017-08-09 09:37:35 -0500

    added last name function from order-by to group by and refactored

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -4,3 +4,10 @@ select distinct *
 from titles
 group by title
 order by title;
+
+select *
+from employees
+where last_name like "e%"
+	and last_name like "%e"
+	group by last_name
+	order by emp_no desc; 
