6e1f851d517ba7d9f5b0ab0f979898a2cc5d77ec
Emmett Peters <ejp8611@gmail.com>
2017-08-09 10:27:26 -0500

    finished part 2 of limit exercise

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -5,6 +5,13 @@ from employees
 order by last_name desc
 limit 10;
 
+select *
+from employees
+where (hire_date like "199%"
+	and birth_date like "%-12-25")
+	order by birth_date, hire_date desc
+	limit 5;
+
 
 
 
