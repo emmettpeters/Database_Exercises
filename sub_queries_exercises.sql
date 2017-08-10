6aad225b05112cdc0d2f64188ac4a41ae69ceff7
Emmett Peters <ejp8611@gmail.com>
2017-08-10 10:48:32 -0500

    finished first and second bonus

--- sub_queries_exercises.sql
+++ sub_queries_exercises.sql
@@ -16,4 +16,20 @@ where emp_no in (
 	select emp_no from dept_manager where gender = "F" and to_date > now()
 );
 
+select dept_name
+from departments
+where dept_no in 
+(
+	select dept_no from dept_manager where emp_no in 
+	(
+		select emp_no from employees where gender = "F" and to_date > now()
+	)
+);
+
+select birth_date, count(birth_date)
+from employees
+group by birth_date
+order by count(birth_date) desc
+limit 1;
+
 
